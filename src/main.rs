7b5f7543f5e5b728ad2cf8a18defb656b494f746
Garrett Moseke <gmosedev@proton.me>
2025-07-05 09:22:10 +0200

    feat: add clap dependency, define CLI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
+use clap::{Parser, Subcommand};
 use rusqlite::Connection;
 use std::io::{BufReader, prelude::*};
 use std::net::TcpStream;
 use std::{env, fs};
 
 mod daemon;
+
+#[derive(Debug, Parser)]
+#[command(version, about="Rediscover your muisc.", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    SurpriseMe {
+        #[command(subcommand)]
+        opt: SurpriseMeCommand,
+
+        #[arg(short, long, default_value_t = false)]
+        same_artist: bool,
+    },
+    Stats,
+    Daemon,
+}
+
+#[derive(Debug, Subcommand)]
+enum SurpriseMeCommand {
+    Album {
+        #[arg(short, long, help = "Number of albums to queue up")]
+        count: Option<u16>,
+    },
+    Playlist {
+        #[arg(short, long, help = "Length of playlist to build, in minutes")]
+        target_length: Option<f32>,
+    },
+}
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect(
@@ -24,7 +58,7 @@ fn main() -> std::io::Result<()> {
     }
 
     let data_path = env::var("XDG_DATA_HOME")
-        .unwrap_or(env::var("HOME").expect("No Data path found") + "/.local/share/")
+        .unwrap_or(env::var("HOME").expect("Home env var not set") + "/.local/share/")
         + "eurydice/";
     fs::create_dir_all(&data_path)?;
     let db = Connection::open(data_path + "db.db3").expect("Could not open db connection");
@@ -33,12 +67,30 @@ fn main() -> std::io::Result<()> {
         Err(e) => panic!("Failed db initialization: {:?}", e),
     }
 
-    loop {
-        let new_song = daemon::wait_for_song_change(&mut stream);
-        daemon::handle_song_change(new_song, &db);
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Stats => {
+            println!("stats")
+        }
+        Commands::Daemon => loop {
+            let new_song = daemon::wait_for_song_change(&mut stream);
+            daemon::handle_song_change(new_song, &db);
+        },
+        Commands::SurpriseMe {
+            opt,
+            same_artist,
+        } => match opt {
+            SurpriseMeCommand::Album { count } => {
+                println!("album of count {:?}", count)
+            }
+            SurpriseMeCommand::Playlist { target_length } => {
+                println!("playlist of length {:?}", target_length)
+            }
+        },
     }
-    //
-    // Ok(())
+
+    Ok(())
 }
 
 fn setup_db(db: &Connection) -> std::result::Result<(), rusqlite::Error> {
